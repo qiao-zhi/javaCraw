346d28ced7476465a4718d4e72e6f3fb6e5efba4
qiaozhi <qiao_liqiang@163.com>
2018-04-20 18:59:49 +0800

    修改了bat注释

--- 项目源码/start.bat
+++ 项目源码/start.bat
@@ -1,15 +1,15 @@
-#  @表示隐藏它后面的这一行命令本身，只印象当前行
-#  echo off表示它后面的索引命令都不显示命令，只显示结果，可以用echo on打开
+rem @表示隐藏它后面的这一行命令本身，只印象当前行
+rem  echo off表示它后面的索引命令都不显示命令，只显示结果，可以用echo on打开
 @echo off
 
-# 设置classpath，引入项目依赖的jar包
+rem 设置classpath，引入项目依赖的jar包
 set classpath=.;%cd%\cn\qlq\craw\lib\*
 
-# 编译源代码
+rem 编译源代码
 javac .\cn\qlq\craw\JsoupCrawJWXT\*.java
 
-# 执行主程序，开始进行爬虫 
+rem 执行主程序，开始进行爬虫 
 java cn.qlq.craw.JsoupCrawJWXT.MainClass
 
-# 执行完毕之后不关机窗口，用户按下任意键之后关闭窗口
+rem 执行完毕之后不关机窗口，用户按下任意键之后关闭窗口
 pause
